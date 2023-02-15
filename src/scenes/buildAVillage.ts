68c1f28c38a4a40b344e09047469490821edd125
尹志超 <yinzhichao@yinzhichaodebijibendiannao.local>
2023-02-16 07:58:29 +0800

    [modify][what]:添加房屋

--- src/scenes/buildAVillage.ts
+++ src/scenes/buildAVillage.ts
@@ -1,4 +1,4 @@
-import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Sound, Vector3 } from "@babylonjs/core";
+import { ArcRotateCamera, Color3, Engine, HemisphericLight, MeshBuilder, Scene, Sound, StandardMaterial, Texture, Tools, Vector3 } from "@babylonjs/core";
 import yqsnSoundUrl from "../../assets/sound/rxxw_yqsn.mp3";
 import { CreateSceneClass } from "../createScene";
 
@@ -16,16 +16,46 @@ export class BuildAVillage implements CreateSceneClass {
         const sound = new Sound("sound", yqsnSoundUrl ,scene, null, {loop:true, autoplay:true});
         //setInterval(() => sound.play(), 3000);
         
-        const box = MeshBuilder.CreateBox("box", {width: 2, height: 1.5, depth: 3}, scene);
-        box.scaling.x = 2;
-        box.scaling.y = 1.5;
-        box.scaling.z = 3;
-        // const box = MeshBuilder.CreateBox("box", {}, scene);
-        // box.scaling = new Vector3(2, 1.5, 3);
+        const box1 = MeshBuilder.CreateBox("box1", {width: 2, height: 1.5, depth: 3}, scene);
+        box1.position.y = 0.75;
 
-        box.position.y = 1;
+        const box2 = MeshBuilder.CreateBox("box2", {}, scene);
+        box2.scaling.x = 2;
+        box2.scaling.y = 1.5;
+        box2.scaling.z = 3;
+        box2.position = new Vector3(-3, 0.75, 0);
+        
 
-        const ground = MeshBuilder.CreateGround("ground", {width:10, height:10}, scene);
+        const box3 = MeshBuilder.CreateBox("box3", {}, scene);
+        box3.scaling = new Vector3(2, 1.5, 3);
+        box3.position.x = 3;
+        box3.position.y = 0.75;
+        box3.position.z = 0;
+        //box3.rotation.y = Math.PI / 4;
+        box3.rotation.y = Tools.ToRadians(45);
+
+        const box4 = MeshBuilder.CreateBox("box4", {}, scene);
+        box4.position.x = 6;
+        box4.position.y = 0.5;
+        box4.position.z = 0;
+        const box4Materil = new StandardMaterial("Box4Materil", scene);
+        box4Materil.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/floor.png");
+        box4.material = box4Materil;
+
+        const roof4 = MeshBuilder.CreateCylinder("roof4", {diameter: 1.3, height: 1.2, tessellation: 3}, scene);
+        roof4.scaling.x = 0.75;
+        roof4.rotation.z = Math.PI / 2;
+        roof4.position.x = 6;
+        roof4.position.y = 1.22;
+        const roof4Materil = new StandardMaterial("Roof4Materil",scene);
+        roof4Materil.diffuseTexture = new Texture("https://assets.babylonjs.com/environments/roof.jpg");
+        roof4.material = roof4Materil;
+
+        const ground = MeshBuilder.CreateGround("ground", {width:20, height:10}, scene);
+        const groundMateril = new StandardMaterial("GroundMateril", scene);
+        //groundMateril.diffuseColor = new Color3(0, 1, 0);
+        groundMateril.diffuseColor = Color3.Green();
+        ground.material = groundMateril;
         return scene;
     };
 }
